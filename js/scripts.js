1ea93328f0ea39032c73a7ff85b60ec005986876
AaronTooTall <146299049+AaronTooTall@users.noreply.github.com>
2023-12-03 20:40:54 -0500

    info from teacher

--- js/scripts.js
+++ js/scripts.js
@@ -10,9 +10,11 @@ var mainNav = document.querySelector("#mainNav");
 
 var burgerBtn = document.querySelector("#burger-container button");
 
+var signInContainer = document.querySelector("#signIn-container");
+
 var canYouSeeMainNav = false;
 
-console.log(burgerBtn);
+//console.log(burgerBtn);
 
 
 //var sectionTags = document.querySelectorAll("section");
@@ -20,16 +22,19 @@ console.log(burgerBtn);
 
 
 function showNav(){
-    console.log("button clicked");
+    //console.log("button clicked");
     if(canYouSeeMainNav === false){
         //this will add the class of showMainNav to the id mainNav
         mainNav.classList.add("showMainNav");
+
+        signInContainer.classList.add("showSignIn");
+
         canYouSeeMainNav = true;
     }else{
         console.log("hide menu")
         mainNav.classList.remove("showMainNav");
         //this will remove the class of showMainNav to the id mainNav
-        signInContainer.classList.remove("showSignIn")
+        signInContainer.classList.remove("showSignIn");
         canYouSeeMainNav = false;
     }
     
@@ -49,8 +54,12 @@ burgerBtn.addEventListener("click", showNav);
 
 var signInBtn = document.querySelector("#sign-in-btn");
 
+var signInContainer = document.querySelector("#sign-in-container");
+
 function showSignInModal(){
-    console.log("show modal now");
+    //console.log("show modal now");
+
+    signInContainer.classList.add("show-signIn-container");
 
 }
 
